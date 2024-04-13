11de4e6e2315f2a17e5155c5876ccaa8fe36bcc3
Dan Lyu <dan@muddy.ca>
2024-04-13 16:46:19 -0400

    Short -> trail down

--- gsp/place.go
+++ gsp/place.go
@@ -99,6 +99,9 @@ func PlaceGrid(strategy Strategy, initialUSDT float64, leverage int) error {
 		payload.StopLowerLimit = *strategy.StrategyParams.StopLowerLimit
 		payload.StopTriggerType = "MARK_PRICE"
 	}
+	if strategy.Direction == SHORT {
+		payload.TrailingDown = true
+	}
 	s, _ := json.Marshal(payload)
 	discord.Orderf(discord.Json(string(s)))
 	if config.TheConfig.Paper {
