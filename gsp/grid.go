3986df501a7390dd4822e4f29d54946c6b70aec9
Dan Lyu <dan@muddy.ca>
2024-04-17 19:12:23 -0400

    Add oop

--- gsp/grid.go
+++ gsp/grid.go
@@ -299,8 +299,12 @@ func (grid *Grid) String() string {
 		return fmt.Sprintf("%s: %d/%d/%.1f%%", setType, currentSD, sdWhenOpen, ratio*100)
 	}
 	realized, _ := strconv.ParseFloat(grid.GridProfit, 64)
-	return fmt.Sprintf("*%d*, Realized: %.2f, Total: %.2f, **%.2f%%**%s, %s, %s, %s",
+	outOfRange := ""
+	if !grid.MarketPriceWithinRange() {
+		outOfRange = " **[OOR]**"
+	}
+	return fmt.Sprintf("*%d*, Realized: %.2f, Total: %.2f, %s**%.2f%%**%s, %s, %s, %s",
 		grid.GID,
-		realized, grid.TotalPnl, grid.LastRoi*100, extendedProfit,
+		realized, grid.TotalPnl, outOfRange, grid.LastRoi*100, extendedProfit,
 		formatSDRatio(SDRaw), formatSDRatio(SDFiltered), formatSDRatio(SDPairSpecific))
 }
