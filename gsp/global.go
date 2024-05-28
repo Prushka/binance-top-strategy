3a4fad7d2a14ec3e8d9c2b8207a8692d23a91c2d
Dan Lyu <dan@muddy.ca>
2024-05-27 23:42:32 -0400

    Update TrackedGrids

--- gsp/global.go
+++ gsp/global.go
@@ -5,12 +5,14 @@ import mapset "github.com/deckarep/golang-set/v2"
 var SessionCancelledGIDs = mapset.NewSet[int]()
 
 var GGrids = &TrackedGrids{
-	Shorts:          mapset.NewSet[int](),
-	Longs:           mapset.NewSet[int](),
-	Neutrals:        mapset.NewSet[int](),
-	ExistingSIDs:    mapset.NewSet[int](),
-	ExistingSymbols: mapset.NewSet[string](),
-	GridsByGid:      make(map[int]*Grid),
+	Shorts:           mapset.NewSet[int](),
+	Longs:            mapset.NewSet[int](),
+	Neutrals:         mapset.NewSet[int](),
+	ExistingSIDs:     mapset.NewSet[int](),
+	ExistingSymbols:  mapset.NewSet[string](),
+	GridsByGid:       make(map[int]*Grid),
+	TotalGridInitial: map[string]float64{},
+	TotalGridPnl:     map[string]float64{},
 }
 var Bundle *StrategiesBundle
 
