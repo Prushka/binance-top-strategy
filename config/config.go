04346c0d114d1aa5264f21f1154d142a082adc0b
Dan Lyu <dan@muddy.ca>
2024-06-02 10:43:22 -0400

    Tick every 3 min

--- config/config.go
+++ config/config.go
@@ -40,7 +40,7 @@ type Config struct {
 	TakeProfitsBlockMinutes           []int     `env:"TAKE_PROFITS_BLOCK_MINUTES" envDefault:"40,-1,-1,-1"`
 	StopLossNotPickedHrs              []int     `env:"STOP_LOSS_NOT_PICKED_HRS" envDefault:"1,2,3,4,5,6"`
 	StopLossNotPicked                 []float64 `env:"STOP_LOSS_NOT_PICKED" envDefault:"0,-0.05,-0.1,-0.15,-0.2,-0.3"`
-	TickEverySeconds                  int       `env:"TICK_EVERY_SECONDS" envDefault:"300"`
+	TickEverySeconds                  int       `env:"TICK_EVERY_SECONDS" envDefault:"180"`
 	MaxUSDTChunks                     int       `env:"MAX_USDT_CHUNKS" envDefault:"4"`
 	MaxUSDCChunks                     int       `env:"MAX_USDC_CHUNKS" envDefault:"1"`
 	MinInvestmentPerChunk             float64   `env:"MIN_INVESTMENT_PER_CHUNK" envDefault:"10"`
