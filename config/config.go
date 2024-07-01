1a324537d3509268927d84847a2e78e81c037ee9
Dan Lyu <dan@muddy.ca>
2024-06-30 22:58:27 -0400

    Update tp and sl

--- config/config.go
+++ config/config.go
@@ -29,8 +29,8 @@ type Config struct {
 	Reserved                       float64   `env:"RESERVED" envDefault:"0.10"`
 	MaxPerChunk                    float64   `env:"MAX_PER_CHUNK" envDefault:"-1"`
 	TradingBlockMinutesAfterCancel int       `env:"TRADING_BLOCK_MINUTES_AFTER_CANCEL" envDefault:"3"`
-	TakeProfits                    []float64 `env:"TAKE_PROFITS" envDefault:"0.6,0.34,0.27,0.2"`
-	TakeProfitsMaxLookBackMinutes  []int     `env:"TAKE_PROFITS_MAX_LOOKBACK_MINUTES" envDefault:"10,15,25,40"`
+	TakeProfits                    []float64 `env:"TAKE_PROFITS" envDefault:"0.52,0.295,0.235,0.175,0.15"`
+	TakeProfitsMaxLookBackMinutes  []int     `env:"TAKE_PROFITS_MAX_LOOKBACK_MINUTES" envDefault:"10,15,25,40,50"`
 	TakeProfitsBlockMinutes        []int     `env:"TAKE_PROFITS_BLOCK_MINUTES" envDefault:"40,-1,-1,-1"`
 	TickEverySeconds               int       `env:"TICK_EVERY_SECONDS" envDefault:"30"`
 	MaxUSDTChunks                  int       `env:"MAX_USDT_CHUNKS" envDefault:"5"`
@@ -40,7 +40,7 @@ type Config struct {
 	Mode                           string    `env:"MODE" envDefault:"trading"`
 	PreferredLeverage              int       `env:"PREFERRED_LEVERAGE" envDefault:"20"`
 	MaxLeverage                    int       `env:"MAX_LEVERAGE" envDefault:"60"`
-	StopLossMarkForRemoval         []float64 `env:"STOP_LOSS_MARK_FOR_REMOVAL" envDefault:"-0.35,-0.8"`
+	StopLossMarkForRemoval         []float64 `env:"STOP_LOSS_MARK_FOR_REMOVAL" envDefault:"-0.3,-0.7"`
 	StopLossMarkForRemovalSLAt     []float64 `env:"STOP_LOSS_MARK_FOR_REMOVAL_SL_AT" envDefault:"0,-0.1"`
 	NeutralRangeDiff               float64   `env:"NEUTRAL_RANGE_DIFF" envDefault:"0.25"`
 	ShortRangeDiff                 float64   `env:"SHORT_RANGE_DIFF" envDefault:"0.2"`
@@ -59,5 +59,5 @@ func Init() {
 }
 
 func GetNormalized(pf float64, leverage int) float64 {
-	return (pf / 25) * float64(leverage)
+	return (pf / 20) * float64(leverage)
 }
